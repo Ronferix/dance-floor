d4ba85c7a26ff99f2d82e9024213011bb1189ba4
Gilberto Atzayacatl Vargas Torres <66646747+Ronferix@users.noreply.github.com>
2021-10-02 14:43:25 -0500

    Changed the folder for one with more elements
    The new folder has some scripts for movement for the player and the lights.

---
+++ Dance King/Assets/Minigame 1/Assets/Lights/RandomRotationOnSpotLights.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class RandomRotationOnSpotLights : MonoBehaviour
+{
+    [SerializeField] private float speed = 1;
+    [SerializeField] private RotateMode rotateMode = default;
+    [SerializeField] private Vector2 xRange = Vector2.zero;
+    [SerializeField] private Vector2 zRange = default(Vector2);
+    [SerializeField] private Vector2 yRange = default;
+
+    private void Start()
+    {
+        LookToRandomRotation();
+    }
+
+    private void LookToRandomRotation()
+    {
+        var newX = Random.Range(xRange.x, xRange.y);
+        var newZ = Random.Range(zRange.x, zRange.y);
+        var newY = Random.Range(yRange.x, yRange.y);
+
+        transform.DOLocalRotate(new Vector3(newX, newY, newZ), speed, rotateMode).SetSpeedBased(true).OnComplete(LookToRandomRotation);
+    }
+}
---
+++ Dance King/Assets/Minigame 1/Assets/Player/MovePlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    [Header("CONFIGURATIONS")]
+    [SerializeField] private float speed = 1;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        if (horizontal == 0 && vertical == 0)
+            return;
+
+        var direction = new Vector3(horizontal, 0, vertical);
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+}
